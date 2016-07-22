eb60947a6663f5b8e0f951882f2dfda82ac55b0e
Edgar Ruiz <ed.ruiz1106@gmail.com>
2016-07-22 10:39:47 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentification : function(req,res,next){
+		console.log('Private route hit');
+		next();
+	},
+	logger : function(req,res,next){
+		console.log('Request: ' + req.method +' ' + new Date().toString());
+		next();
+	}
+}
+
+
+
+app.get('/MAGA',middleware.logger,function(req,res){
+	res.send('BECAUSE WE ARE GOING TO MAKE AMERICA GREAT AGAIN, WE\'RE GONNA MAKE IT GREATER THAN EVER BEFORE!');
+});
+
+app.use(express.static(__dirname + "/public"),function(){
+	console.log('server started'); 
+});
+
+
+app.listen(PORT);
